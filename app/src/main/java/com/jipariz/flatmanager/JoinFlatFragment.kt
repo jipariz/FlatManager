910e401b2a414d39a46e9f52da8a9fe7834001cb
Vit Nakladal <xnaklad@aisa.fi.muni.cz>
2020-04-17 21:58:23 +0200

    activity_flat_choose layout changed to fragment_flat_choose

--- app/src/main/java/com/jipariz/flatmanager/JoinFlatFragment.kt
+++ app/src/main/java/com/jipariz/flatmanager/JoinFlatFragment.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.jipariz.flatmanager.R
 
 class JoinFlatFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_join_flat, container, false)
+        return inflater.inflate(R.layout.fragment_flat_choose, container, false)
     }
 }
