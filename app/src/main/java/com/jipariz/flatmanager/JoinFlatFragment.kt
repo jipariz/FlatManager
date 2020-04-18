d94d42418b0c447498be4dfd4dd8e8a064fccdb6
Vit Nakladal <xnaklad@aisa.fi.muni.cz>
2020-04-18 15:56:20 +0200

    posledni zmeny

--- app/src/main/java/com/jipariz/flatmanager/JoinFlatFragment.kt
+++ app/src/main/java/com/jipariz/flatmanager/JoinFlatFragment.kt
@@ -1,32 +1,28 @@
 package com.jipariz.flatmanager
 
-import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.jipariz.flatmanager.databinding.FragmentFlatChooseBinding
 
 
 class JoinFlatFragment : Fragment(R.layout.fragment_flat_choose) {
-    private lateinit var binding: FragmentJoinFlatBinding
+    private lateinit var binding: FragmentFlatChooseBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view: View = inflater.inflate(
-            R.layout.fragment_flat_choose,
-            container, false
-        )
-        val button: Button = view.findViewById<View>(R.id.create_new_flat_button) as Button
-        button.setOnClickListener(object : View.OnClickListener {
-            fun onClick(v: View?) {
-                // do something
-            }
-        })
-        return inflater.inflate(R.layout.fragment_flat_choose, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentFlatChooseBinding.bind(view)
+        binding.createNewFlatButton.setOnClickListener {
+            createNewFlat()
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun createNewFlat() {
+        Log.v("ahoj", "pepo")
+        Toast.makeText(context, "AHAAA", Toast.LENGTH_LONG).show()
     }
 }
