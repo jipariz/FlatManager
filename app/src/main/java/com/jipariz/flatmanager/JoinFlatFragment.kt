af2a8ecddb67620512f5f0cf0a7a4a09c97d0db6
Vit Nakladal <xnaklad@aisa.fi.muni.cz>
2020-04-18 14:16:06 +0200

    moje dosavadní snahy - NEFUNGUJE

--- app/src/main/java/com/jipariz/flatmanager/JoinFlatFragment.kt
+++ app/src/main/java/com/jipariz/flatmanager/JoinFlatFragment.kt
@@ -1,18 +1,32 @@
 package com.jipariz.flatmanager
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class JoinFlatFragment : Fragment() {
+
+class JoinFlatFragment : Fragment(R.layout.fragment_flat_choose) {
+    private lateinit var binding: FragmentJoinFlatBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view: View = inflater.inflate(
+            R.layout.fragment_flat_choose,
+            container, false
+        )
+        val button: Button = view.findViewById<View>(R.id.create_new_flat_button) as Button
+        button.setOnClickListener(object : View.OnClickListener {
+            fun onClick(v: View?) {
+                // do something
+            }
+        })
         return inflater.inflate(R.layout.fragment_flat_choose, container, false)
     }
 }
